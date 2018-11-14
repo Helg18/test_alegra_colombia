1c0de384bba29099a025ad258bced62954ccfe37
Henry Leon <helg18@gmail.com>
2018-11-14 08:49:09 -0400

    Improves lists view

--- resources/views/ingredients/list.blade.php
+++ resources/views/ingredients/list.blade.php
@@ -1,20 +1,24 @@
 @extends('layouts.main')
 @section('body')
-    <table class="table table-bordered table-hover">
-        <thead>
-        <td>ID</td>
-        <td>Ingredient</td>
-        </thead>
-        <tbody>
-        @foreach($ingredients as $ingredient)
-            <tr>
-                <td>{{ $ingredient->id }}</td>
-                <td>{{ $ingredient->name }}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-    <div>
-        <a href="{{route('home')}}" class="btn btn-warning">Back</a>
+    <div class="container">
+        <div class="col-lg-8 col-md-8 col-sm-8">
+            <table class="table table-bordered table-hover">
+                <thead>
+                <td>ID</td>
+                <td>Ingredient</td>
+                </thead>
+                <tbody>
+                @foreach($ingredients as $ingredient)
+                    <tr>
+                        <td>{{ $ingredient->id }}</td>
+                        <td>{{ $ingredient->name }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+            <div>
+                <a href="{{route('home')}}" class="btn btn-warning">Back</a>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/orders/list.blade.php
+++ resources/views/orders/list.blade.php
@@ -1,28 +1,32 @@
 @extends('layouts.main')
 @section('body')
-    <table class="table table-bordered table-hover">
-        <thead>
-        <tr>
-            <td>ID</td>
-            <td>Plate</td>
-            <td>Status</td>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($orders as $order)
-            <tr>
-                <td>{{ $order->id }}</td>
-                @if($order->getPlateName())
-                    <td>{{ $order->getPlateName() }}</td>
-                @else
-                    <td>Unknown</td>
-                @endif
-                <td>{{ $order->status }}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-    <div>
-        <a href="{{route('home')}}" class="btn btn-warning">Back</a>
+    <div class="container">
+        <div class="col-lg-8 col-md-8 col-sm-8">
+            <table class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Plate</td>
+                    <td>Status</td>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($orders as $order)
+                    <tr>
+                        <td>{{ $order->id }}</td>
+                        @if($order->getPlateName())
+                            <td>{{ $order->getPlateName() }}</td>
+                        @else
+                            <td>Unknown</td>
+                        @endif
+                        <td>{{ $order->status }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+            <div>
+                <a href="{{route('home')}}" class="btn btn-warning">Back</a>
+            </div>
+        </div>
     </div>
 @endsection
